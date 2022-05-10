38ad0895b6c206bd3a51786bb7a1c2c2756236a2
Asad Darain <asaddarain8@gmail.com>
2022-05-10 23:18:41 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-img': "url('/src/assets/bg-hero.jpg')"
+      }
+    },
+  plugins: [],
+}
+}
